be1040dab5db708b55a36e66ab8ddb5cac9bdd76
aaaalan <aaaalancheng@gmail.com>
2022-09-17 11:15:47 -0700

    line formatting

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,15 +13,9 @@ function Home() {
             <h1 className="mt-5">hello. my name is alan</h1>
           </div>
           <div className="row home-intro-paragraph">
-            <div className="row">
-              <p>I am a writer, director, actor, and editor</p>
-            </div>
-            <div className="row">
-              <p style={{whiteSpace: 'nowrap'}}>I would also like to be a musician, voice actor, illustrator, animator, content creator, streamer, pro gamer, game developer, sound designer, rapper, magician, podcaster, documentarian</p>
-            </div>
-            <div className="row">
-              <p>Welcome to my site, I hope you like what you see</p>
-            </div>
+            <p>I am a writer, director, actor, and editor<br/></p>
+            <p style={{whiteSpace: 'nowrap'}}>I would also like to be a musician, voice actor, illustrator, animator, content creator, streamer, pro gamer, game developer, sound designer, rapper, magician, podcaster, documentarian<br/></p>
+            <p>Welcome to my site, I hope you like what you see<br/></p>
           </div>
         </div>
         <hr/>
