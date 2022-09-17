32339c4981ad94d32fb2aa0913c75eb81c4101a0
aaaalan <aaaalancheng@gmail.com>
2022-09-17 11:10:50 -0700

    home text changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,9 +13,15 @@ function Home() {
             <h1 className="mt-5">hello. my name is alan</h1>
           </div>
           <div className="row home-intro-paragraph">
-            <p>I am a writer, director, actor, and editor</p>
-            <p style={{whiteSpace: 'nowrap'}}>I would also like to be a musician, voice actor, illustrator, animator, content creator, streamer, pro gamer, game developer, sound designer, rapper, magician, podcaster, documentarian</p>
-            <p>Welcome to my site, I hope you like what you see</p>
+            <div className="row">
+              <p>I am a writer, director, actor, and editor</p>
+            </div>
+            <div className="row">
+              <p style={{whiteSpace: 'nowrap'}}>I would also like to be a musician, voice actor, illustrator, animator, content creator, streamer, pro gamer, game developer, sound designer, rapper, magician, podcaster, documentarian</p>
+            </div>
+            <div className="row">
+              <p>Welcome to my site, I hope you like what you see</p>
+            </div>
           </div>
         </div>
         <hr/>
