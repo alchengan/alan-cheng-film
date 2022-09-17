7450bf22c3c3a7966f160b39819248bf3eb7c5e8
aaaalan <aaaalancheng@gmail.com>
2022-09-17 11:30:50 -0700

    hope this works

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,9 +13,9 @@ function Home() {
             <h1 className="mt-5">hello. my name is alan</h1>
           </div>
           <div className="row home-intro-paragraph">
-            <p>I am a writer, director, actor, and editor<br/></p>
-            <p style={{whiteSpace: 'nowrap'}}>I would also like to be a musician, voice actor, illustrator, animator, content creator, streamer, pro gamer, game developer, sound designer, rapper, magician, podcaster, documentarian<br/></p>
-            <p>Welcome to my site, I hope you like what you see<br/></p>
+            <p>I am a writer, director, actor, and editor</p>
+            <p style={{whiteSpace: 'nowrap'}}>I would also like to be a musician, voice actor, illustrator, animator, content creator, streamer, pro gamer, game developer, sound designer, rapper, magician, podcaster, documentarian</p>
+            <p>Welcome to my site, I hope you like what you see</p>
           </div>
         </div>
         <hr/>
